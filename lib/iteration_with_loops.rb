1a9fc4f63e9922e5cb26cba9691601dcc7b30174
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-27 19:32:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,8 @@
 def find_even_values(src)
   count = 0 
-  while count < src[count].length do
+  while count < src.length do
     inner_count = 0
-    while inner_count < src[count][inner_count].length do
+    while inner_count < src[count].length do
       src[count][inner_count] % 2 == 0
     inner_count += 1
     end
